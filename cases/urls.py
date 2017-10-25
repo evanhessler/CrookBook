b234f48ee5ce8d10ff2b8695186566e30e92fa5c
Rony Aguilar <raguil12@lion.lmu.edu>
2017-10-25 12:54:09 -0700

    changing static location

--- cases/urls.py
+++ cases/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'test/', views.test, name = 'test'),
     url(r'office/(?P<office_id>[0-9]+)/', views.office_detail, name='office_detail'),
 ]
