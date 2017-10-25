e2eabf7c441ea891bfbb1eebc6c763de8e14d3de
Max Williams <mwill104@lion.lmu.edu>
2017-10-25 15:57:53 -0700

    Removed extra static file dir and renamed index to homepage

--- CrookBook/settings.py
+++ CrookBook/settings.py
@@ -126,5 +126,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "cases/static"),
 )
--- cases/urls.py
+++ cases/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name = 'index'),
+    url(r'^$', views.homepage, name = 'homepage'),
     url(r'office/(?P<office_id>[0-9]+)/', views.office_detail, name='office_detail'),
 ]
