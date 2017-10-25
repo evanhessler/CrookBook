de0110a5dc16067cfede271df0e08e4685000dce
Rony Aguilar <raguil12@lion.lmu.edu>
2017-10-25 14:10:39 -0700

    Changed static directories and linked to index page

--- cases/urls.py
+++ cases/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'test/', views.test, name = 'test'),
+    url(r'^$', views.index, name = 'index'),
     url(r'office/(?P<office_id>[0-9]+)/', views.office_detail, name='office_detail'),
 ]
--- cases/views.py
+++ cases/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Office
 
+def index(request):
+    return render(request, 'index.html',{})
+
 def office_detail(request, office_id):
     office = get_object_or_404(Office, id=office_id)
     return render(request, 'office/detail.html', {'office': office})
