5553509cf0a90fbc79e1ee91bac4bf02072eca6b
Nicholas Sunga <nick.sunga95@gmail.com>
2017-10-30 18:39:55 -0700

    jquery tings for info-buttons when clicked

--- cases/static/javascript/styles.js
+++ cases/static/javascript/styles.js
@@ -1,3 +1,42 @@
 $(document).ready(function () {
 	console.log("In styles.js");
+
+	var navSelected = $('#case-info-button');
+
+	$('#case-info-button').click(function() {
+		navSelected.removeClass('nav-selected');
+		$(this).addClass('nav-selected');
+		navSelected = $(this);
+  });
+
+	$('#binder-info-button').click(function() {
+		navSelected.removeClass('nav-selected');
+		$(this).addClass('nav-selected');
+		navSelected = $(this);
+	});
+
+	$('#district-info-button').click(function() {
+		navSelected.removeClass('nav-selected');
+		$(this).addClass('nav-selected');
+		navSelected = $(this);
+	});
+
+	$('#victims-info-button').click(function() {
+		navSelected.removeClass('nav-selected');
+		$(this).addClass('nav-selected');
+		navSelected = $(this);
+	});
+
+	$('#suspects-info-button').click(function() {
+		console.log('inside binder click');
+		navSelected.removeClass('nav-selected');
+		$(this).addClass('nav-selected');
+		navSelected = $(this);
+	});
+
+	$('#crime-info-button').click(function() {
+		navSelected.removeClass('nav-selected');
+		$(this).addClass('nav-selected');
+		navSelected = $(this);
+	});
 });
