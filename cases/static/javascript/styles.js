6dcd7e4f3f25a8a5b5f8452279f8426fb40d008d
Evan Hessler <ehessler@lion.lmu.edu>
2017-10-30 20:20:04 -0700

    add entry form jquery added

--- cases/static/javascript/styles.js
+++ cases/static/javascript/styles.js
@@ -1,30 +1,44 @@
 $(document).ready(function () {
-	console.log("In styles.js");
-
 	var navSelected = $('#case-info-button');
 
 	$('#case-info-button').click(function() {
 		navSelected.removeClass('nav-selected');
 		$(this).addClass('nav-selected');
 		navSelected = $(this);
-  });
+		$('.block-selected').each(function() {
+			$(this).removeClass('block-selected');
+		});
+		$('#case-info-entry').addClass('block-selected');
+  	});
 
 	$('#binder-info-button').click(function() {
 		navSelected.removeClass('nav-selected');
 		$(this).addClass('nav-selected');
 		navSelected = $(this);
+		$('.block-selected').each(function() {
+			$(this).removeClass('block-selected');
+		});
+		$('#binder-info-entry').addClass('block-selected');
 	});
 
 	$('#district-info-button').click(function() {
 		navSelected.removeClass('nav-selected');
 		$(this).addClass('nav-selected');
 		navSelected = $(this);
+		$('.block-selected').each(function() {
+			$(this).removeClass('block-selected');
+		});
+		$('#district-info-entry').addClass('block-selected');
 	});
 
 	$('#victims-info-button').click(function() {
 		navSelected.removeClass('nav-selected');
 		$(this).addClass('nav-selected');
 		navSelected = $(this);
+		$('.block-selected').each(function() {
+			$(this).removeClass('block-selected');
+		});
+		$('#victims-info-entry').addClass('block-selected');
 	});
 
 	$('#suspects-info-button').click(function() {
@@ -32,11 +46,19 @@ $(document).ready(function () {
 		navSelected.removeClass('nav-selected');
 		$(this).addClass('nav-selected');
 		navSelected = $(this);
+		$('.block-selected').each(function() {
+			$(this).removeClass('block-selected');
+		});
+		$('#suspects-info-entry').addClass('block-selected');
 	});
 
 	$('#crime-info-button').click(function() {
 		navSelected.removeClass('nav-selected');
 		$(this).addClass('nav-selected');
 		navSelected = $(this);
+		$('.block-selected').each(function() {
+			$(this).removeClass('block-selected');
+		});
+		$('#crime-info-entry').addClass('block-selected');
 	});
 });
