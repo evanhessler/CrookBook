d5962ea2cd715131ff4a725d9e57b91dbf3ced2b
Nicholas Sunga <nick.sunga95@gmail.com>
2017-12-08 22:26:04 -0800

    if case dr is the same: POST works for all changes except binder and persons

--- cases/views.py
+++ cases/views.py
@@ -207,7 +207,7 @@ def detail(request, case_id):
                 case.binders.add(binder)
             #
             #
-            
+
             if victim_valid:
                 victim = victim_form.save()
                 case.victims.add(victim)
@@ -219,13 +219,157 @@ def detail(request, case_id):
                 event = event_form.save(commit=False)
                 event.case = case
                 event.save()
+                existing_case.delete()
+                return HttpResponseRedirect('/detail/' + case.dr_nbr)
 
-            existing_case.delete()
-            return HttpResponseRedirect('/detail/' + case.dr_nbr)
+            return render(request, '400-bad-request.html', {
+                'case_form': case_form,
+                'binder_form': binder_form,
+                'district_form': district_form,
+                'victim_form': victim_form,
+                'suspect_form': suspect_form,
+                'event_form': event_form,
+            })
         else:
             print('hey')
-            render(request, '400-bad-request.html', {})
+            # TODO: Update case fields instead
+            case_valid = case_form.is_valid() #only invalid if it currently exists or is too long
+            district_valid = district_form.is_valid()
+            binder_valid = binder_form.is_valid()
+            event_valid = event_form.is_valid()
+            victim_valid = victim_form.is_valid()
+            suspect_valid = suspect_form.is_valid()
+            dr_number_error = None
+            more_case_errors = False
 
+            existing_district = None
+            existing_binder = None
+
+            if not case_valid:
+                for field in case_form.fields:
+                    if field in case_form.errors:
+                        if field == 'dr_nbr':
+                            dr_number_error = True
+                        else:
+                            more_case_errors = True
+                            case_valid = False
+
+            if dr_number_error and not more_case_errors and not case_valid: #update the case, its a valid form, just dont touch DR
+                case_valid = True
+                for field in case_form.fields:
+                    if field != 'dr_nbr' and field != 'evidence_destroyed':
+                        print(str(field) + " : " + str(case_form[field].value()))
+                        setattr(existing_case, field, case_form[field].value() or None)
+                    elif field == 'evidence_destroyed':
+                        if case_form[field].value():
+                            boolean_value = True
+                            setattr(existing_case, field, True)
+                        else:
+                            boolean_value = False
+                            setattr(existing_case, field, False)
+            else:
+                print('your case is not fukn valid')
+            # ENSURES IF THE FORM DISTRICT IS VALID.
+            # IF NEW, ADD IT TO DB AND CASE, ELSE, ADD THE EXISTING ONE TO CASE
+            all_districts = District.objects.all()
+            verify = 0
+            for obj in all_districts:
+                for field in district_form.fields:
+                    if getattr(obj, field) == district_form[field].value():
+                        verify+=1
+                        if verify == 3:
+                            district_does_exist = True
+                            obj_primary_key = obj.pk
+                            break
+                    else:
+                        district_does_exist = False
+                        verify = 0
+                        break
+                if verify == 3:
+                    break
+
+            print('about to do district')
+            if verify == 3 and district_does_exist:
+                add_new_district = False
+            elif not district_does_exist and district_valid and case_valid and verify == 0:
+                add_new_district = True
+
+            if add_new_district and district_valid and case_valid:
+                district = district_form.save()
+                existing_case.district = district
+            elif not add_new_district and district_valid and case_valid:
+                existing_case.district = District.objects.get(id=obj_primary_key)
+            #
+            #
+
+            # ENSURES IF THE BINDER FORM IS VALID.
+            # IF NEW, ADD IT TO DB AND CASE, ELSE, ADD THE EXISTING ONE TO CASE
+            try:
+                existing_binder = Binder.objects.get(master_dr=request.POST['binder-master_dr'])
+                binder_valid = True
+            except Binder.DoesNotExist:
+                existing_binder = None # then create a new binder
+
+            if binder_valid and existing_binder != None:
+                already_linked = existing_case.binders.filter(master_dr=existing_binder.master_dr).exists() #check if its already there. if not, add it, else IDK
+                if not already_linked:
+                    binder_relationships = existing_case.binders.all() # remove the current one?
+                    for binder in binder_relationships:
+                        existing_case.binders.remove(binder)
+                    existing_case.binders.add(existing_binder)
+            elif binder_valid and existing_binder == None:
+                binder = binder_form.save()
+                existing_case.binders.add(binder)
+            #
+            #
+
+            # try to ensure these guys are different tho?
+            if victim_valid:
+                same_guy = False
+                victim_relationships = existing_case.victims.all()
+                for victim in victim_relationships:
+                    for field in victim_form.fields:
+                        if getattr(victim, field) == victim_form[field].value():
+                            same_guy = True
+                        else:
+                            same_guy = False
+                            break
+
+                if not same_guy:
+                    victim = victim_form.save()
+                    existing_case.victims.add(victim)
+            if suspect_valid:
+                same_guy = False
+                suspect_relationships = existing_case.suspects.all()
+                for suspect in suspect_relationships:
+                    for field in suspect_form.fields:
+                        if getattr(suspect, field) == suspect_form[field].value():
+                            same_guy = True
+                        else:
+                            same_guy = False
+                            break
+
+                if not same_guy:
+                    suspect = suspect_form.save()
+                    existing_case.suspects.add(suspect)
+
+            if event_valid:
+                existing_event = get_object_or_404(Event, case = existing_case)
+                for field in event_form.fields:
+                        print(str(field) + " : " + str(event_form[field].value()))
+                        setattr(existing_event, field, event_form[field].value() or None)
+                existing_event.save()
+                existing_case.save()
+                return HttpResponseRedirect('/detail/' + existing_case.dr_nbr)
+
+            return render(request, '400-bad-request.html', {
+                'case_form': case_form,
+                'binder_form': binder_form,
+                'district_form': district_form,
+                'victim_form': victim_form,
+                'suspect_form': suspect_form,
+                'event_form': event_form,
+            })
 
 
 
